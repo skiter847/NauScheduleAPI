c664ea5b5f6e41fef55bfc35ac0451757f9c157c
artem <ivashenco45@gmail.com>
2020-09-27 23:14:25 +0300

    [bugfix] decided bug with incorrect department name in get_schedule method

--- nauschedule/__init__.py
+++ nauschedule/__init__.py
@@ -29,39 +29,54 @@ class NAUScheduler:
     def get_group_course(group_number: str):
         return group_number[0]
 
+    def _correct_department_name(self, dep_name):
+        department_codes = self.get_department_codes()
+        try:
+            return True, department_codes[dep_name]
+        except KeyError:
+            return False, 'Неправильное название факультета.'
+
     def get_schedule(self, department_name: str, group_number: str, subgroup=1, only_lecture=None, only_practice=None):
         department_codes = self.get_department_codes()
         group_code = self.get_group_stream_and_code(group_number)
         group_stream = group_code
         group_course = self.get_group_course(group_number)
 
-        url = f'{self._group_scheduler_url}/{department_codes[department_name]}/{group_course}/{group_stream}/{group_code}/{subgroup}'
-        response = requests.get(url).json()
+        correct_department_name = self._correct_department_name(department_name)
 
-        if response['status']:
+        if correct_department_name[0]:
 
-            if only_lecture and only_practice:
-                return 'Допустимо выбрать только 1 флаг, если вам нужно получить все пары - пропустите установку флагов.'
+            url = f'{self._group_scheduler_url}/{department_codes[department_name]}/{group_course}/{group_stream}/{group_code}/{subgroup}'
+            response = requests.get(url).json()
 
-            elif only_lecture:
-                lectures = {}
-                for date in response['schedule']:
-                    if response['schedule'][date]['isLecture']:
-                        lectures.update({date: response['schedule'][date]})
-                return lectures
+            if response['status']:
 
-            elif only_practice:
-                practices = {}
-                for date in response['schedule']:
-                    print(date)
-                    if not response['schedule'][date]['isLecture']:
-                        practices.update({date: response['schedule'][date]})
-                return practices
+                if only_lecture and only_practice:
+                    return 'Допустимо выбрать только 1 флаг, если вам нужно получить все пары - пропустите установку флагов.'
+
+                elif only_lecture:
+                    lectures = {}
+                    for date in response['schedule']:
+                        if response['schedule'][date]['isLecture']:
+                            lectures.update({date: response['schedule'][date]})
+                    return lectures
+
+                elif only_practice:
+                    practices = {}
+                    for date in response['schedule']:
+                        print(date)
+                        if not response['schedule'][date]['isLecture']:
+                            practices.update({date: response['schedule'][date]})
+                    return practices
+
+                else:
+                    return response['schedule']
 
             else:
-                return response['schedule']
-
+                return response['message']
         else:
-            return response['message']
+            return correct_department_name[1]
 
-
+if __name__ == '__main__':
+    nau = NAUScheduler()
+    print(nau.get_schedule('ФАЕТ', '05'))
